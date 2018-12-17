0c4d98b247c9fd3bd0fa687c519d64bbeb0a8ea6
wfb <1142768306@qq.com>
2018-12-18 01:32:46 +0800

    添加五个服装模态框图片

--- pages/end/end.js
+++ pages/end/end.js
@@ -1,39 +1,5 @@
 Page({
   data: {
-    roleList: [{
-      desc: '出品人Producer',
-      name: '刘馨亦Angela'
-    },{
-      desc: '监制Executive Producer',
-      name: '刘倚彤Fiona'
-    },{
-      desc: '策划&编辑Production&Editor',
-      name: '芒果mango'
-    },{
-      desc: '艺人统筹Artist Adminitration',
-      name: '刘倚彤Fiona'
-    },{
-      desc: '摄影 Photograph',
-      name: 'Edwin Zhang'
-    },{
-      desc: '视频团队Film Team',
-      name: '王聪Cong'
-    },{
-      desc: '造型Styulist',
-      name: '大圆圆Rita'
-    },{
-      desc: '发型Hair',
-      name: 'Kevin'
-    },{
-      desc: '现场摄影SetPhotographer',
-      name: '关爱子Rebecca'
-    },{
-      desc: '文案编辑Junior Editor',
-      name: '王毅文Winnne'
-    },{
-      desc: '图片制作photographEditor',
-      name: '常馨予Cici'
-    }],
     animationData: {},
   },
   onShow: function () {
