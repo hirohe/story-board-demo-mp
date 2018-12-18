66008f1386eb62d4043cb580fc7fc5c454d9d997
wfb <1142768306@qq.com>
2018-12-18 18:03:07 +0800

    update

--- components/signImage2/signImage2.js
+++ components/signImage2/signImage2.js
@@ -12,6 +12,7 @@ Component({
    * 组件的初始数据
    */
   data: {
+    signStatus: true
   },
 
   ready() {
@@ -23,5 +24,8 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    hideSign() {
+      this.setData({signStatus: false})
+    }
   }
 });
