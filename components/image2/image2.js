26fbbb672b5de7098a7b3c952acfc0df0d1f3911
wfb <1142768306@qq.com>
2018-12-20 13:28:34 +0800

    update

--- components/image2/image2.js
+++ components/image2/image2.js
@@ -1,3 +1,4 @@
+const moving_threshold = 50;
 Component({
   options: {
     multipleSlots: true
@@ -12,6 +13,10 @@ Component({
    * 组件的初始数据
    */
   data: {
+    startX: null,
+    movingX: null,
+    moving: false,
+    animationData: null,
   },
 
   ready() {
@@ -25,9 +30,60 @@ Component({
   methods: {
     onNext() {
       console.log('sign image 1 on next');
-      return true;
+      return this.canMove;
     },
-    onImageLoad() {
-    }
+    fullImageMove(e) {
+      const { startX, moving } = this.data;
+      const touchData = e.touches[0];
+
+      if (!this.data.moving) {
+        this.setData({ startX: touchData.clientX, moving: true });
+      } else {
+        const movingX = touchData.clientX - startX;
+        this.setData({ movingX });
+      }
+      this.canMove = false;
+    },
+    fullImageEnd(e) {
+      const { movingX } = this.data;
+
+      if (Math.abs(movingX) > moving_threshold) {
+        this.setData({ startX: null, movingX: null, moving: false });
+        if (movingX < 0) {
+          // 下一页,去做动画效果
+          this.imageToSmall();
+        } else {
+          // 上一页
+          this.canMove = true;
+        }
+      }
+    },
+    // 图片变小
+    imageToSmall() {
+      const animation = wx.createAnimation({
+        duration: 1500,
+      });
+      animation.opacity(0).step();
+      this.setData({
+        animationData: animation.export()
+      });
+    },
+
+    // 图片变大
+    imageToBig() {
+      const animation = wx.createAnimation({
+        duration: 100,
+      });
+
+      animation.scaleY(1).step();
+
+      this.setData({
+        animationData: animation.export()
+      });
+    },
+    onTransitionEnd() {
+      this.canMove = true;
+      this.triggerEvent('nextComponent');
+    },
   }
 });
---
+++ components/imageScale2/imageScale2.js
@@ -0,0 +1,38 @@
+Component({
+  options: {
+    multipleSlots: true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+  },
+
+  ready() {
+    // this.isCanMove = true;
+  },
+  detached() {
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onNext() {
+      console.log('sign image 1 on next');
+      return this.canMove;
+    },
+    onRootTouchStart() {
+      this.canMove = true;
+    },
+    fontMove(e) {
+      this.canMove = false;
+    },
+  }
+});
